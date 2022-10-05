f2da11534058b0d0824c1d5ff50a94e30fb83d43
Jakub <jkob.matras@gmail.com>
2022-10-05 12:44:08 +0200

    First commit

---
+++ src/main/java/com/example/etutorbackend/ETutorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.etutorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ETutorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ETutorBackendApplication.class, args);
+    }
+
+}
