0fd081bb7a80491f1e2af3e5cdb169158fef6b33
Jakub <jkob.matras@gmail.com>
2022-10-06 11:07:42 +0200

    Added entities for cities and states

---
+++ src/main/java/com/example/etutorbackend/model/entity/City.java
@@ -0,0 +1,34 @@
+package com.example.etutorbackend.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import static javax.persistence.CascadeType.*;
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "city")
+public class City {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne(cascade = {
+            DETACH, MERGE, PERSIST, REFRESH
+    })
+    @JoinColumn(name = "state_id", referencedColumnName = "id")
+    private State state;
+
+}
---
+++ src/main/java/com/example/etutorbackend/model/entity/State.java
@@ -0,0 +1,41 @@
+package com.example.etutorbackend.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.CascadeType.ALL;
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "state")
+public class State {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(cascade = ALL, mappedBy = "state")
+    private List<City> cities = new ArrayList<>();
+
+}
+
+
+    
+
+
+
+
