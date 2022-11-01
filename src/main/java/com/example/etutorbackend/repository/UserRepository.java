959eac3fb2b51ad9cf7e8c732b99048118322ae7
pozerinhooo <jkob.matras@gmail.com>
2022-11-01 17:40:09 +0100

    Fixed query bug

--- src/main/java/com/example/etutorbackend/repository/UserRepository.java
+++ src/main/java/com/example/etutorbackend/repository/UserRepository.java
@@ -17,7 +17,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(@Param("username") String username);
 
     @Query(value = "SELECT DISTINCT user.id, user.address, user.city, user.created_at, user.email, user.enabled, user.first_name,\n" +
-            "user.last_name, user.last_updated, user.password, user.phone_number, user.profile_image_path, user.username FROM user, message\n" +
-            "WHERE (message.receiver_id = :userId OR message.sender_id = :userId) AND user.id != :userId", nativeQuery = true)
+            "            user.last_name, user.last_updated, user.password, user.phone_number, user.profile_image_path, user.username FROM user, message\n" +
+            "            WHERE message.sender_id = user.id \n" +
+            "            AND (message.receiver_id = :userId OR message.sender_id = :userId) AND user.id != :userId", nativeQuery = true)
     Page<User> findUsersForConversationByUserId(@Param("userId") Long userId, Pageable pageable);
 }
